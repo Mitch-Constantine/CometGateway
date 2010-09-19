784139b563250087fdd9cced83c4c57e5c96f2af
Mitch Constantinescu <Mitch.Constantine@gmail.com>
2010-09-19 15:22:46 +0000

    Just empty projects

---
+++ Src/CometGateway.Server/CometGateway.Server.Gateway/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CometGateway.Server.Gateway
+{
+    public class Class1
+    {
+    }
+}
---
+++ Src/CometGateway.Server/CometGateway.Server.TelnetDemo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CometGateway.Server.TelnetDemo.MvcApplication" Language="C#" %>
